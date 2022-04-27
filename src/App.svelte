910f0d3ec4f6ca28c1e1417fd5dc7460740e2832
Rian Perassoli <rian.perassoli@gmail.com>
2022-04-27 11:06:40 -0300

    simple test with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+	let a = 1;
+	let b = 2;
+	let result = 0;
+
+	function sum(value1, value2) {
+		return Number(value1) + Number(value2);
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<input type="number" bind:value={a} />
+	<input type="number" bind:value={b} />
+
+	<p>{sum(a, b)}</p>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
